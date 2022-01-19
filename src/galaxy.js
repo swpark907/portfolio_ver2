1214ea176c657f3cf17aecb3894abc74d64bc6c2
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-19 23:59:02 +0900

    update create meteor function

--- src/galaxy.js
+++ src/galaxy.js
@@ -1,32 +1,50 @@
-const moon = document.querySelector('.moon');
-const body = document.querySelector('body');
 const area = document.querySelector('.galaxy')
 
 function galaxy() {
   let amountMeteor = 15;
-  let amountStar = 300;
+  let amountStar = 1000;
   let i = 0;
   let j = 0;
-  while(i < amountMeteor){
+  let k = 0;
+
+
+  while(i < amountMeteor){ // meteor from top
     let drop = document.createElement('i');
     let size = Math.random() * 2 + 0.5;
     let posX = Math.floor(Math.random() * window.innerWidth);
-    let delay = Math.random() * -20;
-    let duration = Math.random() * 5 + 5;
+    let delay = Math.random() * - 20;
+    let duration = Math.random() * 20 + 10;
 
     drop.style.width = size + 'px';
     drop.style.left = posX + 'px';
+    drop.style.top = 0;
     drop.style.animationDelay = delay + 's';
     drop.style.animationDuration = duration + 's';
     area.appendChild(drop);
     i++;
   }
 
+  while(k < amountMeteor*2){ // meteor from right
+    let drop = document.createElement('i');
+    let size = Math.random() * 2 + 0.5;
+    let posY = Math.floor(Math.random() * window.innerHeight * 4);
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 5 + 5;
+
+    drop.style.width = size + 'px';
+    drop.style.right = 0;
+    drop.style.top = posY + 'px';
+    drop.style.animationDelay = delay + 's';
+    drop.style.animationDuration = duration + 's';
+    area.appendChild(drop);
+    k++;
+  }
+
   while(j < amountStar){
     let star = document.createElement('j');
     let size = Math.random() * 5;
     let posX = Math.floor(Math.random() * window.innerWidth*3);        
-    let posY = Math.floor(Math.random() * window.innerHeight);
+    let posY = Math.floor(Math.random() * window.innerHeight * 5);
     let delay = Math.random() * 100 + 50;
     let duration = Math.random() * 300 + 15;
 
