65283a7b0324e205afb9c500adb3b9cb4770ba00
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-21 17:19:40 +0900

    copied email when email box clicked

--- src/copy.js
+++ src/copy.js
@@ -1,7 +1,23 @@
 'use strict'
 
 const emailBox = document.querySelector('.email-box');
+const email = document.querySelector('.email');
+const confirmation = document.querySelector('.confirmation');
 
 emailBox.addEventListener('click', (e) => {
-  
+
+  document.execCommand("copy");
+
+  email.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", email.textContent);
+    console.log(event.clipboardData.getData("text"))
+    
+    confirmation.classList.add('on');
+    setTimeout(() => {
+      confirmation.classList.remove('on');
+    }, 1000)
+  }
+});
 })
